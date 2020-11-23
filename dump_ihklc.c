e9ac2aa40b4d25abda3991cb6c91342058ef8ab2
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2020-11-23 16:01:46 +0100

    tank 1 and 2

--- dump_ihklc.c
+++ dump_ihklc.c
@@ -43,7 +43,8 @@ typedef struct {
 } dataspec;
 
 dataspec dataspecs[]={
-			{.name="TCS__TH__TANK1__TOP",.hrw="INTL-SVM2-HRW",.cnv="INTL-SVM2-CNV",.col="TCS__TH__TANK1__TOP"},
+			{.name="TCS__TH__TANK1__TOP",.hrw="INTL-SVM2-CNV",.cnv="INTL-SVM2-CNV",.col="TCS__TH__TANK1__TOP"},
+			{.name="TCS__TH__TANK2__TOP",.hrw="INTL-SVM1-CNV",.cnv="INTL-SVM1-CNV",.col="TCS__TH__TANK2__TOP"},
 			{.name="IBIS_VETO",.hrw="IBIS-DPE.-HRW",.cnv="IBIS-DPE.-CNV",.col="V1S_MBOT_MCOUNT"},
             {.name="IBIS_VETO_LAT",.hrw="IBIS-DPE.-HRW",.cnv="IBIS-DPE.-CNV",.col="V1S_MLAT_MCOUNT"},
             {.name="ISGRIRAW_MCE0",.hrw="IBIS-DPE.-HRW",.cnv="IBIS-DPE.-CNV",.col="I0S_MEVTCNT_MMCE0"},
